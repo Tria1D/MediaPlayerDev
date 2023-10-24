9c20e632463030247bd249f29bd3687bc9584856
Tria1D <tuanduy2000w@gmail.com>
2023-10-25 00:33:21 +0700

    init project with Media Service

---
+++ app/src/main/java/vn/trialapp/mediaplayerdev/utils/AppConstants.kt
@@ -0,0 +1,10 @@
+package vn.trialapp.mediaplayerdev.utils
+
+class AppConstants {
+
+    object MediaNotification {
+        const val NOTIFICATION_ID = 200
+        const val NOTIFICATION_CHANNEL_NAME = "notification channel 1"
+        const val NOTIFICATION_CHANNEL_ID = "notification channel id 1"
+    }
+}
